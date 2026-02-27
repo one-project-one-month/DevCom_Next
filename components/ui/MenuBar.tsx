5691edeace07f5021496d0d06ec06cdb76a45330
htayhtaythwe <htayhtaythwe962@gmail.com>
2026-02-27 14:48:56 +0700

    update & solve error

--- components/ui/MenuBar.tsx
+++ components/ui/MenuBar.tsx
@@ -1,18 +1,17 @@
-"use client"; 
+"use client";
 import type { Editor } from "@tiptap/core";
 import { useEditorState } from "@tiptap/react";
 import { menuBarStateSelector } from "./menuBarState";
 
 export const MenuBar = ({ editor }: { editor: Editor | null }) => {
-  if (!editor) {
-    return null;
-  }
-
   const editorState = useEditorState({
     editor,
     selector: menuBarStateSelector,
   });
 
+  if (!editor) {
+    return null;
+  }
 
   return (
     <div className="control-group border rounded-md focus-within:ring-2 focus-within:ring-blue-500 focus-within:border-blue-500">
@@ -20,28 +19,36 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
         <button
           onClick={() => editor.chain().focus().toggleBold().run()}
           disabled={!editorState.canBold}
-          className={editorState.isBold ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isBold ? "bg-blue-500 text-white p-1 rounded-sm" : ""
+          }
         >
           Bold
         </button>
         <button
           onClick={() => editor.chain().focus().toggleItalic().run()}
           disabled={!editorState.canItalic}
-          className={editorState.isItalic ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isItalic ? "bg-blue-500 text-white p-1 rounded-sm" : ""
+          }
         >
           Italic
         </button>
         <button
           onClick={() => editor.chain().focus().toggleStrike().run()}
           disabled={!editorState.canStrike}
-          className={editorState.isStrike ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isStrike ? "bg-blue-500 text-white p-1 rounded-sm" : ""
+          }
         >
           Strike
         </button>
         <button
           onClick={() => editor.chain().focus().toggleCode().run()}
           disabled={!editorState.canCode}
-          className={editorState.isCode ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isCode ? "bg-blue-500 text-white p-1 rounded-sm" : ""
+          }
         >
           Code
         </button>
@@ -53,7 +60,11 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
         </button>
         <button
           onClick={() => editor.chain().focus().setParagraph().run()}
-          className={editorState.isParagraph ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isParagraph
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           Paragraph
         </button>
@@ -61,7 +72,11 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 1 }).run()
           }
-          className={editorState.isHeading1 ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isHeading1
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           H1
         </button>
@@ -69,7 +84,11 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
-          className={editorState.isHeading2 ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isHeading2
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           H2
         </button>
@@ -77,7 +96,11 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
-          className={editorState.isHeading3 ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isHeading3
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           H3
         </button>
@@ -85,7 +108,11 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 4 }).run()
           }
-          className={editorState.isHeading4 ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isHeading4
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           H4
         </button>
@@ -93,7 +120,11 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 5 }).run()
           }
-          className={editorState.isHeading5 ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isHeading5
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           H5
         </button>
@@ -101,31 +132,51 @@ export const MenuBar = ({ editor }: { editor: Editor | null }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 6 }).run()
           }
-          className={editorState.isHeading6 ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isHeading6
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           H6
         </button>
         <button
           onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editorState.isBulletList ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isBulletList
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           Bullet list
         </button>
         <button
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editorState.isOrderedList ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isOrderedList
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           Ordered list
         </button>
         <button
           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          className={editorState.isCodeBlock ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isCodeBlock
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           Code block
         </button>
         <button
           onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={editorState.isBlockquote ? "bg-blue-500 text-white p-1 rounded-sm" : ""}
+          className={
+            editorState.isBlockquote
+              ? "bg-blue-500 text-white p-1 rounded-sm"
+              : ""
+          }
         >
           Blockquote
         </button>
