e139a9f64fdc0f7743e4204c04337da850979bb4
hhs <htethlaingswan@gmail.com>
2026-03-02 10:20:41 +0630

    Fix explore page Suspense error

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { ExplorePageWrapper } from "@/app/explore/_components/explore-page-wrapper";
 
 export default function ExplorePage() {
-  return <ExplorePageWrapper />;
+  return (
+    <Suspense fallback={null}>
+      <ExplorePageWrapper />
+    </Suspense>
+  );
 }
